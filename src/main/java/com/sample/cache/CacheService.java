edc8d7b9265a97055625541026253e87487f845d
Srividya <43784342+srividrs@users.noreply.github.com>
2021-01-15 15:09:33 +0530

    Update CacheService.java

--- src/main/java/com/sample/cache/CacheService.java
+++ src/main/java/com/sample/cache/CacheService.java
@@ -1,19 +1,3 @@
-/*
- * @author srividya.ramaswamy
- * @since Jan 12, 2021
- *
- * COPYRIGHT NOTICE:
- * Copyright (c) 2009 Infosys Technologies Limited, Electronic City,
- * Hosur Road, Bangalore - 560 100, India.
- * All Rights Reserved.
- * This software is the confidential and proprietary information of
- * Infosys Technologies Ltd. ("Confidential Information"). You shall
- * not disclose such Confidential Information and shall use it only
- * in accordance with the terms of the license agreement you entered
- * into with Infosys.
- */
-
-
 package com.sample.cache;
 
 import java.util.UUID;
